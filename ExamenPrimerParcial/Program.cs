9d86d00ce03c474b17a389b3d151be1819977b51
Morapx <bryan_alexis_1999@hotmail.com>
2019-09-05 12:14:18 -0700

    Final

--- ExamenPrimerParcial/Program.cs
+++ ExamenPrimerParcial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ExamenPrimerParcial
 {
@@ -6,17 +7,51 @@ namespace ExamenPrimerParcial
     {
         static void Main(string[] args)
         {
-            List<playlist> playlist = new List<playlist>();
+            List<Playlist> playlist = new List<Playlist>();
 
-            playlist playlist1 = new playlist("Familiar", "Peliculas para toda la familia");
-            playlist playlist2 = new playlist("Mayor", "Peliculas recomendadas a mayores de 12");
+            Playlist playlist1 = new Playlist("1)Familiar", "Peliculas para toda la familia");
+            Playlist playlist2 = new Playlist("2)Mayor", "Peliculas recomendadas a mayores de 12");
 
             playlist.Add(playlist1);
             playlist.Add(playlist2);
 
-            playlist1.playlistdepeliculas.Add(new Peliculas("Nemo", "2000", "Pepe", "USA", "Pez pequeño perdido", "Aventura", "8", "220"));
+            playlist1.playlistdepeliculas.Add(new Peliculas("Nemo", 2000, "Pepe", "USA", "Pez pequeño perdido", "Aventura", 8.9, 220));
+            playlist1.playlistdepeliculas.Add(new Peliculas("spiderman", 2006, "Ricardo", "Mexico", "Un hombre que araña", "Accion", 6.5, 150));
+            playlist1.playlistdepeliculas.Add(new Peliculas("Aladin", 1997, "Jaime", "India", "Un hombre con un esclavo azul", "Aventura y Romance", 7.3, 160));
+            playlist1.playlistdepeliculas.Add(new Peliculas("Goofy", 2000, "Milos", "USA", "Un hombre perro que vive su dia dia", "Comedia", 9.6, 130));
+            playlist1.playlistdepeliculas.Add(new Peliculas("Donald", 1999, "JOTARO", "japon", "Pato blanco que no se le entiende nada", "Comedia Accion", 8.2, 120));
+            playlist1.playlistdepeliculas.Add(new Peliculas("Minions", 2014, "DIO", "Egipto", "Platanos vivientes", "Aventura y comedia", 1.5, 100));
+            playlist1.playlistdepeliculas.Add(new Peliculas("spiderman", 2006, "Ricardo", "Mexico", "Un hombre que araña", "Accion", 6.3, 190));
 
+            playlist2.playlistdepeliculas.Add(new Peliculas("Matrix", 2001, "Karen", "Mexico", "Un hombre drogado en un compu", "Accion y Futuristica", 9.6, 200));
+            playlist2.playlistdepeliculas.Add(new Peliculas("Thor", 2010, "Eduardo", "Australia", "men que lanza truenos de las manos", "Accion y Aventura", 8.6, 160));
+            playlist2.playlistdepeliculas.Add(new Peliculas("Merlin", 2011, "Ruben", "Mexico", "Un hombre magico", "Magia", 5.6, 160));
+            playlist2.playlistdepeliculas.Add(new Peliculas("SpaceJam", 1982, "Victor", "USA", "Michael Jordan juega cbasket con caricaturas", "Ficcion y Relatos de la vida", 5.6, 130));
+            playlist2.playlistdepeliculas.Add(new Peliculas("ElOrigen", 2009, "Carlos", "USA", "Mens que entran en sueños", "Accion y Ciencia ficcion", 8.3, 90));
+            playlist2.playlistdepeliculas.Add(new Peliculas("El padrino", 2005, "Jenny", "Grecia", "un men que es padrino", "Accion y misterio", 7.1, 120));
+            playlist2.playlistdepeliculas.Add(new Peliculas("PerdidoEnElEspacio", 1975, "Cambell", "Mexico", "Un hombre perdido en el eswpacio", "Accion, Futuristica y ciencia ficcion", 3.5, 90));
 
+            foreach (Playlist play in playlist)
+            {
+                Console.WriteLine("*" + play.Titulo + "-" + play.Descripcion);
+                foreach (Peliculas peli in play.playlistdepeliculas)
+                {
+                    Console.WriteLine("1) *" + peli.Titulo);
+
+                    try
+                    {
+                        peli.Año = peli.Año;
+                        Console.WriteLine("año:" + peli.Año);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("el año no es correcto");
+                    }
+
+                }
+
+                Console.Read();
+            }
 
         }
     }
--- ExamenPrimerParcial/playlist.cs
+++ ExamenPrimerParcial/playlist.cs
@@ -4,20 +4,20 @@ using System.Text;
 
 namespace ExamenPrimerParcial
 {
-    class playlist
+    class Playlist
     {
        public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public List<Peliculas> playlistdepeliculas { get; set; }
 
-        public playlist()
+        public Playlist()
         {
             Titulo = "No se encuentra titulo";
             Descripcion = "no Se encuentra una descripcion";
             playlistdepeliculas = new List<Peliculas>();
         }
 
-        public playlist(string titulo, string descripcion)
+        public Playlist(string titulo, string descripcion)
         {
             Titulo = titulo;
             Descripcion = descripcion;
@@ -25,4 +25,4 @@ namespace ExamenPrimerParcial
         }
     }
     }
-}
+
